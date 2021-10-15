ae48c42401b0dc38660812eed21d40816a14be52
bochoy <1335172962@qq.com>
2021-10-15 21:01:06 +0800

    create

---
+++ src/test/java/com/bokchoy/nowcode_community/NowcodeCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bokchoy.nowcode_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowcodeCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
